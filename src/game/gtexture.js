e0c5f7bc68a1198c3553ffd8760c71e94add5af1
iswear <471291492@qq.com>
2018-02-11 17:53:08 +0800

    refactor gtexture

--- src/game/gtexture.js
+++ src/game/gtexture.js
@@ -10,48 +10,65 @@ export default (
   function () {
     var functions = {
       syncImg: function () {
+        this.removeObserver('render', functions.renderImg, this, this);
+        this.removeObserver('render', functions.renderImgClip, this, this);
         if (this.img !== null && this.img !== '') {
           if (LangUtil.isString(this.img)) {
             this._img.url = this.img;
-            this._needUpdateImgSize = true;
+            this._img.image = null;
+            this._img.progress = 0;
+            this.addObserver('render', functions.renderImg, this, this);
           } else {
             this._img.url = this.img.url;
-            this._img.x = this.img.x;
-            this._img.y = this.img.y;
-            this._img.width = this.img.width;
-            this._img.height = this.img.height;
-            this.width = this.img.width;
-            this.height = this.img.height;
+            this._img.image = null;
+            this._img.progress = 0;
+            this.addObserver('render', functions.renderImgClip, this, this);
           }
-          if (this.getObserverByAllParams('render', functions.renderImg, this, this) === null) {
-            this.addObserver('render', functions.renderImg, this, this);
-          }
-        } else {
-          this.removeObserver('render', functions.renderImg, this, this);
         }
+      },
+      syncImgRender: function (image) {
+        if (LangUtil.isString(this.img)) {
+          this.width = image.width;
+          this.height = image.height;
+          this._img.image = image;
+        } else {
+          this.width = this.img.width;
+          this.height = this.img.height;
+          this._img.image = image;
+          this._img.x = this.img.x;
+          this._img.y = this.img.y;
+          this._img.width = this.img.width;
+          this._img.height = this.img.height;
+        }
+        this.refresh();
       },
       renderImg: function (sender, render) {
         var img = this._img;
-        var image = this.findApplication().getFileLoader().loadImageAsync(
-          img.url,
-          functions.loadImageFinished,
-          this
-        );
-        if (image !== null) {
-          if (this._needUpdateImgSize) {
-            img.x = 0;
-            img.y = 0;
-            img.width = image.width;
-            img.height = image.height;
-            this.width = img.width;
-            this.height = img.height;
-            this._needUpdateImgSize = false;
-          }
+        if (!img.image) {
+          functions.loadImage.call(this, img.url);
+        } else {
           var rect = this.getRectInSelf();
-          if (LangUtil.isString(this.img)) {
-            render.drawImage(image, img.x, img.y, img.width, img.height, rect.left, rect.top, img.width, img.height);
-          } else {
-            render.drawImage(image, rect.left, rect.top);
+          render.drawImage(img.image, rect.left, rect.top);
+        }
+      },
+      renderImgClip: function (sender, render) {
+        var img = this._img;
+        if (!img.image) {
+          functions.loadImage.call(this, img.url);
+        } else {
+          var rect = this.getRectInSelf();
+          render.drawImageExt(img.image, img.x, img.y, img.width, img.height, rect.left, rect.top, img.width, img.height);
+        }
+      },
+      loadImage: function (url) {
+        var fileLoader = this.findApplication().getFileLoader();
+        var image = fileLoader.loadImageAsync(url);
+        if (image !== null) {
+          functions.syncImgRender.call(this, image);
+        } else {
+          if (this._img.progress === 0) {
+            fileLoader.loadImageAsync(url, functions.loadImageFinished, this);
+            this._img.progress = 1;
           }
         }
       },
@@ -59,12 +76,14 @@ export default (
         if (success) {
           var image = this.findApplication().getFileLoader().loadImageAsync(url);
           if (image !== null) {
-            if (LangUtil.isString(this.img)) {
-              this._needUpdateImgSize = true;
-            }
+            functions.syncImgRender.call(this, image);
+            this._img.progress = 2;
+          } else {
+            this._img.progress = 3;
           }
+        } else {
+          this._img.progress = 3;
         }
-        this.refresh();
       }
     };
 
@@ -78,8 +97,15 @@ export default (
         this.super('init', [conf]);
         this.defineNotifyProperty('img', LangUtil.checkAndGet(conf.img, this.defImg));
 
-        this._img = {};
-        this._needUpdateImgSize = true;
+        this._img = {
+          x: 0,
+          y: 0,
+          width: 0,
+          height: 0,
+          url: null,
+          image: null,
+          progress: 0
+        };
 
         functions.syncImg.call(this);
 
