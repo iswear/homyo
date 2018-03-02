afb79a4298dc68067f150e0b05246c2d474cf5bf
iswear <471291492@qq.com>
2018-03-02 23:49:35 +0800

    优化测试

--- src/game/gmap.js
+++ src/game/gmap.js
@@ -9,79 +9,22 @@ import Node from '../core/node';
 
 export default (
   function () {
-    // var functions = (function () {
-    //   function syncGridVisible () {
-    //     if (this.gridVisible) {
-    //       if (this.getObserverByAllParams('render', renderGrid, this, this) === null) {
-    //         this.addObserver('render', renderGrid, this, this, 1);
-    //       }
-    //     } else {
-    //       this.removeObserver('render', renderGrid, this, this);
-    //     }
-    //   }
-    //   function renderGrid (sender, render) {
-    //     var rect = this.getRectInSelf();
-    //     var left = rect.left;
-    //     var top = rect.top;
-    //     var right = rect.right;
-    //     var bottom = rect.bottom;
-    //     left = left < this.containerLeft ? this.containerLeft : left;
-    //     right = (right < this.containerRight ? right : this.containerRight);
-    //     top = top < this.containerTop ? this.containerTop : top;
-    //     bottom = (bottom < this.containerBottom ? bottom : this.containerBottom);
-    //
-    //     var gridWidth = this.gridWidth;
-    //     var gridHeight = this.gridHeight;
-    //     var x = Math.floor(left / gridWidth + 1) * gridWidth + 0.5, y = Math.floor(top / gridHeight + 1) * gridHeight + 0.5;
-    //     render.beginPath();
-    //     for (; x < right; x += gridWidth) {
-    //       render.moveTo(x, top);
-    //       render.lineTo(x, bottom);
-    //     }
-    //     for (; y < bottom; y += gridHeight) {
-    //       render.moveTo(left, y);
-    //       render.lineTo(right, y);
-    //     }
-    //     render.strokeStyle = this.gridColor;
-    //     render.stroke();
-    //   }
-    //
-    //   return {
-    //     syncGridVisible: syncGridVisible,
-    //     renderGrid: renderGrid
-    //   }
-    // })();
 
     var GMap = (function () {
       var InnerGMap = LangUtil.extend(Node);
 
+      InnerGMap.prototype.defWidth = 0;
+      InnerGMap.prototype.defHeight = 0;
       InnerGMap.prototype.defAnchorX = 0;
       InnerGMap.prototype.defAnchorY = 0;
-      InnerGMap.prototype.defGridVisible = false;
-      InnerGMap.prototype.defGridColor = '#000';
-      InnerGMap.prototype.defGridWidth = 50;
-      InnerGMap.prototype.defGridHeight = 50;
       InnerGMap.prototype.init = function (conf) {
         this.super('init', [conf]);
-        // this.defineNotifyProperty('gridVisible', LangUtil.checkAndGet(conf.gridVisible, this.defGridVisible));
-        // this.defineNotifyProperty('gridColor', LangUtil.checkAndGet(conf.gridColor, this.defGridColor));
-        // this.defineNotifyProperty('gridWidth', LangUtil.checkAndGet(conf.gridWidth, this.defGridWidth));
-        // this.defineNotifyProperty('gridHeight', LangUtil.checkAndGet(conf.gridHeight, this.defGridHeight));
         this.defineNotifyProperty('containerLeft', LangUtil.checkAndGet(conf.containerLeft, -Infinity));
         this.defineNotifyProperty('containerRight', LangUtil.checkAndGet(conf.containerRight, -Infinity));
         this.defineNotifyProperty('containerTop', LangUtil.checkAndGet(conf.containerTop, Infinity));
         this.defineNotifyProperty('containerBottom', LangUtil.checkAndGet(conf.containerBottom, Infinity));
 
         this._models = [];
-
-        // functions.syncGridVisible.call(this);
-
-        // this.addObserver('gridVisibleChanged', this.refresh, this, this);
-        // this.addObserver('gridColorChanged', this.refresh, this, this);
-        // this.addObserver('gridWidthChanged', this.refresh, this, this);
-        // this.addObserver('gridHeightChanged', this.refresh, this, this);
-
-        // this.addObserver('gridVisibleChanged', functions.syncGridVisible, this, this);
       }
 
       InnerGMap.prototype.addModel = function (model, x, y, layerIndex) {
--- src/game/gtiledmap.js
+++ src/game/gtiledmap.js
@@ -12,170 +12,25 @@ export default (
     var doc = document;
 
     var functions = (function () {
-      function syncRenderSize () {
-        this.width = this.gridWidth * this.col;
-        this.height = this.gridHeight * this.row;
-      }
-      function refreshRenderCache () {
-        this._mapRenderCache.cacheInvalid = true;
-      }
-      function renderTiledMap (sender, render) {
-        var mapCache = this._mapRenderCache;
-        if (mapCache.cacheInvalid) {
-          renderTiledMapCache.call(this);
-          mapCache.cacheInvalid = false;
-        }
-        var rect = this.getRectInSelf();
-        var left = rect.left < this.containerLeft ? this.containerLeft : rect.left;
-        var top = rect.top < this.containerTop ? this.containerTop : rect.top;
-        var width = (rect.right < this.containerRight ? rect.right : this.containerRight) - left;
-        var height = (rect.bottom < this.containerBottom ? rect.bottom : this.containerBottom) - top;
-        var x = left - mapCache.leftCol * this.gridWidth;
-        var y = top - mapCache.topRow * this.gridHeight;
-        if (width > 0 && height > 0) {
-          render.drawImageExt(
-            mapCache.cacheFore.getCanvas(), x, y, width, height, left, top, width, height
-          );
-        }
-      }
-      function renderTiledMapCache () {
-        var rect = this.getRectInSelf();
-        var left = rect.left < this.containerLeft ? this.containerLeft : rect.left;
-        var top = rect.top < this.containerTop ? this.containerTop : rect.top;
-        var right = rect.right < this.containerRight ? rect.right : this.containerRight;
-        var bottom = rect.bottom < this.containerBottom ? rect.bottom : this.containerBottom;
-        var gridWidth = this.gridWidth;
-        var gridHeight = this.gridHeight;
-
-        var leftCol = Math.floor(left / this.gridWidth);
-        var rightCol = Math.ceil(right / this.gridWidth);
-        var topRow = Math.floor(top / this.gridHeight);
-        var bottomRow = Math.ceil(bottom / this.gridHeight);
-
-        var mapRenderCache = this._mapRenderCache;
-        if (mapRenderCache.cacheInit) {
-          var cacheLeftCol = mapRenderCache.leftCol;
-          var cacheRightCol = mapRenderCache.rightCol;
-          var cacheTopRow = mapRenderCache.topRow;
-          var cacheBottomRow = mapRenderCache.bottomRow;
-
-          if (!(leftCol >= mapRenderCache.leftCol && rightCol <= mapRenderCache.rightCol
-            && topRow >= mapRenderCache.topRow && bottomRow <= mapRenderCache.bottomRow)) {
-
-            var backRender = mapRenderCache.cacheBack;
-            var foreRender = mapRenderCache.cacheFore;
-
-            var clipLeftCol = Math.max(cacheLeftCol, leftCol);
-            var clipRightCol = Math.min(cacheRightCol, rightCol);
-            var clipTopRow = Math.max(cacheTopRow, topRow);
-            var clipBottomRow = Math.min(cacheBottomRow, bottomRow);
-
-            backRender.width = (rightCol - leftCol + 1) * gridWidth;
-            backRender.height = (bottomRow - topRow + 1) * gridHeight;
-
-            var fileLoader = this.findApplication().getFileLoader();
-            for (var i = topRow, tiledY = 0; i <= bottomRow; ++i, tiledY += gridHeight) {
-              for (var j = leftCol, tiledX = 0; j <= rightCol; ++j, tiledX += gridHeight) {
-                if (i < clipTopRow || i > clipBottomRow || j < clipLeftCol || j > clipRightCol) {
-                  var tiles = this.tiles[i+":"+j];
-                  if (tiles && tiles.length > 0) {
-                    for (var m = 0, len = tiles.length; m < len; ++m) {
-                      var tiled = tiles[m];
-                      var image = fileLoader.loadImageAsync(
-                        tiled.url,
-                        loadImageFinished,
-                        this
-                      );
-                      if (image !== null) {
-                        backRender.drawImageExt(
-                          image, tiled.left, tiled.top, tiled.width, tiled.height, tiledX, tiledY, gridWidth, gridHeight
-                        );
-                      }
-                    }
-                  }
-                }
-              }
-            }
-            if (clipLeftCol <= clipRightCol && clipTopRow <= clipBottomRow) {
-              var clipX = (clipLeftCol - cacheLeftCol) * gridWidth;
-              var clipY = (clipTopRow - cacheTopRow) * gridHeight;
-              var clipWidth = (clipRightCol - clipLeftCol + 1) * gridWidth;
-              var clipHeight = (clipBottomRow - clipTopRow + 1) * gridHeight;
-              if (clipWidth > 0 && clipHeight > 0) {
-                backRender.drawImageExt(
-                  foreRender.getCanvas(), clipX, clipY, clipWidth, clipHeight, (clipLeftCol - leftCol) * gridWidth, (clipTopRow - topRow) * gridHeight, clipWidth, clipHeight
-                );
-              }
-            }
-            foreRender.clear();
-            mapRenderCache.cacheFore = backRender;
-            mapRenderCache.cacheBack = foreRender;
-            mapRenderCache.leftCol = leftCol;
-            mapRenderCache.rightCol = rightCol;
-            mapRenderCache.topRow = topRow;
-            mapRenderCache.bottomRow = bottomRow;
-          }
-        } else {
-          var foreRender = mapRenderCache.cacheFore;
-          foreRender.width = (rightCol - leftCol + 1) * gridWidth;
-          foreRender.height = (bottomRow - topRow + 1) * gridHeight;
-          var fileLoader = this.findApplication().getFileLoader();
-          for (var i = topRow, tiledY = 0; i <= bottomRow; ++i, tiledY += gridHeight) {
-            for (var j = leftCol, tiledX = 0; j <= rightCol; ++j, tiledX += gridWidth) {
-              var tiles = this.tiles[i + ":" + j];
-              if (tiles && tiles.length > 0) {
-                for (var m = 0, len = tiles.length; m < len; ++m) {
-                  var tiled = tiles[m];
-                  var image = fileLoader.loadImageAsync(
-                    tiled.url,
-                    loadImageFinished,
-                    this
-                  );
-                  if (image !== null) {
-                    foreRender.drawImageExt(
-                      image, tiled.left, tiled.top, tiled.width, tiled.height, tiledX, tiledY, gridWidth, gridHeight
-                    );
-                  }
-                }
-              }
-            }
-          }
-          mapRenderCache.cacheInit = true;
-          mapRenderCache.leftCol = leftCol;
-          mapRenderCache.rightCol = rightCol;
-          mapRenderCache.topRow = topRow;
-          mapRenderCache.bottomRow = bottomRow;
-        }
-      }
-      function loadImageFinished () {
-        this._mapRenderCache.cacheInvalid = true;
-        this._mapRenderCache.cacheInit = false;
-        this.refresh();
+      function syncRenderCacheSize() {
+        this._mapRenderCache.cacheFore.width = ;
+        this._mapRenderCache.cacheFore.height = ;
       }
 
       return {
-        syncRenderSize: syncRenderSize,
-        refreshRenderCache: refreshRenderCache,
-        renderTiledMap: renderTiledMap,
-        loadImageFinished: loadImageFinished
+        syncRenderCacheSize: syncRenderCacheSize
       }
     })();
-
+    
     var GTiledMap = (function () {
-      var InnerGTiledMap = LangUtil.extend(GMap);
+      var GInnerTiledMap = LangUtil.extend(GMap);
 
-      InnerGTiledMap.prototype.init = function (conf) {
+      GInnerTiledMap.prototype.init = function (conf) {
         this.super('init', [conf]);
         this.defineNotifyProperty('tileWidth', LangUtil.checkAndGet(conf.tileWidth, 50));
         this.defineNotifyProperty('tileHeight', LangUtil.checkAndGet(conf.tileHeight, 50));
         this.defineNotifyProperty('tileInclineX', LangUtil.checkAndGet(conf.tileInclineX, 0));
         this.defineNotifyProperty('tileInclineY', LangUtil.checkAndGet(conf.tileInclineY, 0));
-
-
-        this.defineNotifyProperty('row', LangUtil.checkAndGet(conf.row, 50));
-        this.defineNotifyProperty('col', LangUtil.checkAndGet(conf.col, 50));
-        this.defineNotifyProperty('brush', LangUtil.checkAndGet(conf.brush, null));
-        this.defineNotifyProperty('tiles', LangUtil.checkAndGet(conf.tiles, {}));
 
         this._mapRenderCache = {
           leftCol: -1,
@@ -186,31 +41,19 @@ export default (
           cacheInit: false,
           cacheFore: new CanvasRender({canvas: doc.createElement('canvas')}),
           cacheBack: new CanvasRender({canvas: doc.createElement('canvas')})
-        };
+        }
 
-        functions.syncRenderSize.call(this);
-        functions.refreshRenderCache.call(this);
+        this.addObserver('tileWidthChanged', this.syncRenderCacheSize, this, this);
+        this.addObserver('tileHeightChanged', this.syncRenderCacheSize, this, this);
+        this.addObserver('tileInclineXChanged', this.syncRenderCacheSize, this, this);
+        this.addObserver('tileInclineYChanged', this.syncRenderCacheSize, this, this);
 
-        this.addObserver('xChanged', functions.refreshRenderCache, this, this);
-        this.addObserver('yChanged', functions.refreshRenderCache, this, this);
-        this.addObserver('widthChanged', functions.refreshRenderCache, this, this);
-        this.addObserver('heightChanged', functions.refreshRenderCache, this, this);
-
-        this.addObserver('containerLeftChanged', functions.refreshRenderCache, this, this);
-        this.addObserver('containerRightChanged', functions.refreshRenderCache, this, this);
-        this.addObserver('containerTopChanged', functions.refreshRenderCache, this, this);
-        this.addObserver('containerBottomChanged', functions.refreshRenderCache, this, this);
-
-        this.addObserver('render', functions.renderTiledMap, this, this);
+        this.addObserver('containerLeftChanged', this.syncRenderCacheSize, this, this);
+        this.addObserver('containerRightChanged', this.syncRenderCacheSize, this, this);
+        this.addObserver('containerTopChanged', this.syncRenderCacheSize, this, this);
+        this.addObserver('containerBottomChanged', this.syncRenderCacheSize, this, this);
       }
 
-      // InnerGTiledMap.prototype.invalidRenderCache = function () {
-      //   this._mapRenderCache.cacheInvalid = true;
-      //   this._mapRenderCache.cacheInit = false;
-      //   this.refresh();
-      // }
-
-      return InnerGTiledMap;
     })();
 
     return GTiledMap;
