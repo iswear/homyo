52e652b06647138182114c6758ceec6fe80f91ca
hantang <hujb@tuya.com>
2018-09-04 17:02:41 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,30 @@
  */
 var path = require('path');
 
-module.exports = {
-  entry: './index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+// module.exports = {
+//   entry: './main.js',
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.resolve(__dirname, 'dist')
+//   }
+// };
+
+module.exports = env => {
+  if (env.profile === 'test') {
+    return {
+      entry: './test/game/main.js',
+      output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist/test')
+      }
+    }
+  } else {
+    return {
+      entry: './main.js',
+      output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist/build')
+      }
+    };
   }
 };
