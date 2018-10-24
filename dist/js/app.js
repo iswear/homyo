59a04037a155e5e3cfcc8394ad8ef6d7be061a0c
iswear <471291492@qq.com>
2018-10-25 01:31:00 +0800

    update

--- dist/js/app.js
+++ dist/js/app.js
@@ -734,13 +734,13 @@
                   node.parent = null;
                   node.application = null;
                   this._childNodes.count--;
-                  node.postNotification('remove', [this, destroy]);
                   return;
                 }
               }
             }
           }
         }
+        node.postNotification('remove', [this, destroy]);
       }
 
       InnerNode.prototype.removeFromParent = function (destroy) {
@@ -3305,8 +3305,15 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
   var Application = __WEBPACK_IMPORTED_MODULE_0__main__["a" /* default */].core.Application;
   var PropertyAnimation = __WEBPACK_IMPORTED_MODULE_0__main__["a" /* default */].core.animation.PropertyAnimation;
   var UILabel = __WEBPACK_IMPORTED_MODULE_0__main__["a" /* default */].ui.Label;
+  var CNode = __WEBPACK_IMPORTED_MODULE_0__main__["a" /* default */].core.Node;
   var GTexture = __WEBPACK_IMPORTED_MODULE_0__main__["a" /* default */].game.Texture;
   var GMap = __WEBPACK_IMPORTED_MODULE_0__main__["a" /* default */].game.Map;
+
+  // var root = new CNode({
+  //   x: 400,
+  //   y: 300,
+  //   visible: true
+  // });
 
   // var root = new UILabel({
   //   x: 400,
@@ -3321,12 +3328,16 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
   //   borderRadius: 10
   // });
 
-  // var root = new GTexture({
-  //   x: 400,
-  //   y: 300,
-  //   visible: true,
-  //   image: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1536657006&di=f6e8dc17d395fd0841a24aa1f068ce3c&imgtype=jpg&er=1&src=http%3A%2F%2Fp2.qhimg.com%2Ft0193dcb0a279f6ec8f.jpg',
-  // });
+  var root = new GTexture({
+    x: 400,
+    y: 300,
+    scaleX: 0.5,
+    // shearY: 1,
+    // scaleY: 1,
+    // rotateZ: -0.3805063771123649,
+    visible: true,
+    image: './images/frame_00015.png',
+  });
 
   // var root = new GMap({
   //   x: 250,
@@ -3374,51 +3385,51 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
   //   ]
   // });
 
-  var root = new GMap({
-    x: 250,
-    y: 250,
-    mapX: 10,
-    mapY: 10,
-    width: 200,
-    height: 200,
-    anchorX: 0.5,
-    anchorY: 0.5,
-    visible: true,
-    mapTileType: 'diamond',
-    mapTileWidth: 30,
-    mapTileHeight: 30,
-    mapTileImageIndex: {
-      1: 'images/203-4.png'
-    },
-    mapTileImageClipIndex: {
-      1: {
-        imageId: 1,
-        x: 0,
-        y: 0,
-        width: 128,
-        height: 64
-      }
-    },
-    mapTileRows: 15,
-    mapTileCols: 15,
-    mapTileData: [
-      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    ]
-  });
+  // var root = new GMap({
+  //   x: 250,
+  //   y: 250,
+  //   mapX: 10,
+  //   mapY: 10,
+  //   width: 200,
+  //   height: 200,
+  //   anchorX: 0.5,
+  //   anchorY: 0.5,
+  //   visible: true,
+  //   mapTileType: 'diamond',
+  //   mapTileWidth: 30,
+  //   mapTileHeight: 30,
+  //   mapTileImageIndex: {
+  //     1: 'images/203-4.png'
+  //   },
+  //   mapTileImageClipIndex: {
+  //     1: {
+  //       imageId: 1,
+  //       x: 0,
+  //       y: 0,
+  //       width: 128,
+  //       height: 64
+  //     }
+  //   },
+  //   mapTileRows: 15,
+  //   mapTileCols: 15,
+  //   mapTileData: [
+  //     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  //     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  //     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  //     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  //     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  //     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  //     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  //     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  //     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  //     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  //     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  //     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  //     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  //     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  //     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  //   ]
+  // });
 
   var application = new Application({
     canvas: document.getElementById('app'),
@@ -3427,35 +3438,35 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
   application.run();
 
-  root.runAnimation(new PropertyAnimation({
-      property: 'rotateZ',
-      offset: Infinity,
-      offsetFn: function (animation, deltaTime, sumTime) {
-          return sumTime / 100;
-      }
-  }), null, null, false);
+  // root.runAnimation(new PropertyAnimation({
+  //     property: 'rotateZ',
+  //     offset: Infinity,
+  //     offsetFn: function (animation, deltaTime, sumTime) {
+  //         return sumTime / 100;
+  //     }
+  // }), null, null, false);
 
-  for (var i = 0; i < 10; ++i) {
-    root.appendChildNode(new GTexture({
-      x: 20 * i,
-      y: 20 * i,
-      visible: true,
-      image: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1536657006&di=f6e8dc17d395fd0841a24aa1f068ce3c&imgtype=jpg&er=1&src=http%3A%2F%2Fp2.qhimg.com%2Ft0193dcb0a279f6ec8f.jpg',
-    }));
-  }
+  // for (var i = 0; i < 10; ++i) {
+  //   root.appendChildNode(new GTexture({
+  //     x: 20 * i,
+  //     y: 20 * i,
+  //     visible: true,
+  //     image: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1536657006&di=f6e8dc17d395fd0841a24aa1f068ce3c&imgtype=jpg&er=1&src=http%3A%2F%2Fp2.qhimg.com%2Ft0193dcb0a279f6ec8f.jpg',
+  //   }));
+  // }
 
-  document.onkeydown = function (e) {
-    var e = e ? event : e;
-    if (e.keyCode === 37) {
-      root.mapX += 10;
-    } else if (e.keyCode === 39) {
-      root.mapX -= 10;
-    } else if (e.keyCode === 38) {
-      root.mapY += 10;
-    } else if (e.keyCode === 40) {
-      root.mapY -= 10;
-    }
-  }
+  // document.onkeydown = function (e) {
+  //   var e = e ? event : e;
+  //   if (e.keyCode === 37) {
+  //     root.mapX += 10;
+  //   } else if (e.keyCode === 39) {
+  //     root.mapX -= 10;
+  //   } else if (e.keyCode === 38) {
+  //     root.mapY += 10;
+  //   } else if (e.keyCode === 40) {
+  //     root.mapY -= 10;
+  //   }
+  // }
 
 })();
 
