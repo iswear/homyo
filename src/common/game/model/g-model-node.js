fad4f3ef32689ebde786576eb6403fc0b31da2a5
iswear <471291492@qq.com>
2018-11-02 01:55:37 +0800

    update

--- src/common/game/model/g-model-node.js
+++ src/common/game/model/g-model-node.js
@@ -8,26 +8,25 @@ import Node from '../../core/node';
 import GTexture from '../g-texture';
 
 export default (function () {
-    var GTextureNode = (function () {
-      var InnerGTextureNode = LangUtil.extend(Node);
+    var GModelNode = (function () {
+      var InnerGModelNode = LangUtil.extend(Node);
 
       InnerGTextureNode.prototype.defLayer = 1;
       InnerGTextureNode.prototype.defAnchorX = 0.5;
       InnerGTextureNode.prototype.defAnchorY = 0.5;
       InnerGTextureNode.prototype.init = function (conf) {
         this.super('init', [conf]);
-
         this._texture = new GTexture(LangUtil.checkAndGet(conf.texture, {}));
         this.appendChildNodeToLayer(this._texture, 0);
       }
 
-      InnerGTextureNode.prototype.getTexture = function (conf) {
+      InnerGModelNode.prototype.getTexture = function (conf) {
         return this._texture;
       }
 
-      return InnerGTextureNode;
+      return InnerGModelNode;
     })();
 
-    return GTextureNode;
+    return GModelNode;
   }
 )();
