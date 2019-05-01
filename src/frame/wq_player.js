39323a0829bb6b9cbc5a353a9df437f13a8a16bc
iswear <471291492@qq.com>
2019-05-01 22:56:35 +0800

    update

--- src/frame/wq_player.js
+++ src/frame/wq_player.js
@@ -1,3 +1,47 @@
+import Application from '../common/core/application'
+import Node from '../common/core/node'
+import View from '../common/ui'
+
 export default (function () {
-  return null;
-})()
+  var app = new Application({
+
+  });
+  var root = new Node({
+    x: 0,
+    y: 0,
+  });
+  var uiRoot = new Node({
+    x: 0,
+    y: 0
+  });
+  var gameRoot = new Node({
+    x: 0,
+    y: 0,
+  });
+
+  var context = {
+    game: {
+      properties: {
+
+      },
+      binder: [
+
+      ]
+    },
+    ui: {
+      yaya: {
+        
+      }
+    },
+    map: {
+
+    },
+    object: {
+
+    }
+  }
+
+  root.addChild(uiRoot);
+  root.addChild(gameRoot);
+  app.run();
+})();
